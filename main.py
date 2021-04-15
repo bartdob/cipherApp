f568c63bd60724ecba66903ece551fded6764f08
Bartosz Dobrosielski <dobry1@pm.me>
2021-04-15 17:26:38 +0200

    add decryt alg

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-def read_notes(request: Request):
+def home(request: Request):
     return templates.TemplateResponse("index.html", {
         'request': request,
     })
@@ -25,29 +25,14 @@ def form_post(request: Request, message: str = Form(...), key: str = Form(...)):
     'key': key })
 
 @app.get("/decrypted", response_class=HTMLResponse)
-def read_notes(request: Request):
+def home(request: Request):
     return templates.TemplateResponse("decrypted.html", {
         'request': request,
     })
 
-@app.post('/')
+@app.post('/decrypted')
 def form_post(request: Request, message: str = Form(...), key: str = Form(...)):
-    result = encrypt(message, key)
-    return templates.TemplateResponse('index.html', context={'request': request, 'result': result,
+    result = decrypted(message, key)
+    return templates.TemplateResponse('decrypted.html', context={'request': request, 'result': result,
     'message': message,
     'key': key })
-
-# async def homepage(request):
-#     return templates.TemplateResponse('index.html', {'request': request})
-
-# routes = [
-#     Route('/', endpoint=homepage),
-#     Mount('/static', StaticFiles(directory='static'), name='static')
-# ]
-
-# app = Starlette(debug=True, routes=routes)
-
-# @app.post("/")
-# def form_post(request: Request, num: string = Form(...)):
-#     result = spell_number(num)
-#     return templates.TemplateResponse('indexś.html', context={'request': request, 'result': result})
