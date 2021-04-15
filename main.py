08cc6077ab64b0cf9efb09e183cb1919e4d6a520
Bartosz Dobrosielski <dobry1@pm.me>
2021-04-15 15:07:38 +0200

    encrypt message algorithm

---
+++ main.py
@@ -0,0 +1,42 @@
+from src.cryp import encrypt, decrypted
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def read_notes(request: Request):
+    # result = encrypt(message)
+    return templates.TemplateResponse("index.html", {
+        'request': request,
+        # 'result': result
+    })
+
+@app.post('/')
+def form_post(request: Request, message: str = Form(...), key: str = Form(...)):
+    result = encrypt(message, key)
+    return templates.TemplateResponse('index.html', context={'request': request, 'result': result,
+    'message': message,
+    'key': key })
+
+# async def homepage(request):
+#     return templates.TemplateResponse('index.html', {'request': request})
+
+# routes = [
+#     Route('/', endpoint=homepage),
+#     Mount('/static', StaticFiles(directory='static'), name='static')
+# ]
+
+# app = Starlette(debug=True, routes=routes)
+
+# @app.post("/")
+# def form_post(request: Request, num: string = Form(...)):
+#     result = spell_number(num)
+#     return templates.TemplateResponse('indexś.html', context={'request': request, 'result': result})
